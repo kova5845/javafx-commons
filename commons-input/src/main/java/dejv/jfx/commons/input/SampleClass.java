7d5fc38b22849fffe5c5468195f81539f071bca2
dejv78 <dejv78@gmail.com>
2015-03-04 12:57:18 +0100

    #10 Added commons-input bundle

---
+++ commons-input/src/main/java/dejv/jfx/commons/input/SampleClass.java
@@ -0,0 +1,9 @@
+package dejv.jfx.commons.input;
+
+/**
+ * <p>
+ * @since 1.1.0
+ * @author dejv78 (dejv78.github.io)
+ */
+public class SampleClass {
+}
---
+++ commons-input/src/test/java/dejv/jfx/commons/input/SampleClassTest.java
@@ -0,0 +1,16 @@
+package dejv.jfx.commons.input;
+
+import org.junit.Test;
+
+/**
+ * <p>
+ * @since 1.1.0
+ * @author dejv78 (dejv78.github.io)
+ */
+public class SampleClassTest {
+
+    @Test
+    public void runTest() {
+
+    }
+}
